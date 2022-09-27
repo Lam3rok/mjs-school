15d8342f50211c84b4cb56a1ae4fbd724a66e786
Lam3rok <jekdyatlov@gmail.com>
2022-09-27 14:33:30 +0300

    $ git config --global user.name "Lam3rok"

--- src/main/java/talks/mjc/HelloWorld.java
+++ src/main/java/talks/mjc/HelloWorld.java
@@ -3,6 +3,5 @@ package main.java.talks.mjc;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("Hello world!%n");
-        System.out.printf("1");
     }
 }
