28739478055f38fed309cada9c8a9c4ed79e38b7
Lam3r-ok <lam3rok@protonmail.com>
2022-09-27 13:51:46 +0300

    $ git config --global user.name "Lam3r-ok"

--- src/main/java/talks/mjc/HelloWorld.java
+++ src/main/java/talks/mjc/HelloWorld.java
@@ -3,5 +3,6 @@ package main.java.talks.mjc;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("Hello world!%n");
+        System.out.printf("1");
     }
 }
